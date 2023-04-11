9acded2ef0fd722876583f11d6820d980c6be2ec
Daniel Apatin <daniel@apatin.ru>
2023-04-11 22:04:25 +0300

    don't notify yourself

--- main.go
+++ main.go
@@ -227,10 +227,12 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 
 									if len(notifyUsers) > 0 {
 										for _, userID := range notifyUsers {
-											_, _ = b.SendMessage(ctx, &bot.SendMessageParams{
-												ChatID: userID,
-												Text:   fmt.Sprintf("%s status updated", text),
-											})
+											if update.CallbackQuery.Sender.ID != userID {
+												_, _ = b.SendMessage(ctx, &bot.SendMessageParams{
+													ChatID: userID,
+													Text:   fmt.Sprintf("%s status updated", text),
+												})
+											}
 										}
 									}
 								}
